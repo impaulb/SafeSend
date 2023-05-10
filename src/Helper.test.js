125558aff6eb13d4b3ed8c43cfc472a89f38e18a
Paul <59670248+impaulb@users.noreply.github.com>
2023-05-09 17:28:38 -0700

    Separated files in compose/message

--- src/Helper.test.js
+++ src/Helper.test.js
@@ -1,4 +1,4 @@
-const redact = require('./Redact.gs');
+const helper = require('./Helper.gs');
 
 // Test 1: Verify that valid SSN numbers are detected correctly.
 test("Verify valid SSN positive matches", () => {
@@ -18,7 +18,7 @@ test("Verify valid SSN positive matches", () => {
     const expected = ['SSN'];
     
     valid_ssn.forEach(ssn => {
-        expect(redact.detect(ssn)).toEqual(expect.arrayContaining(expected));
+        expect(helper.getTokenTypes(ssn)).toEqual(expect.arrayContaining(expected));
     });
 });
 
@@ -37,7 +37,7 @@ test("Verify valid phone positive matches", () => {
     const expected = ['PHONE'];
     
     valid_phone.forEach(phone => {
-        expect(redact.detect(phone)).toEqual(expect.arrayContaining(expected));
+        expect(helper.getTokenTypes(phone)).toEqual(expect.arrayContaining(expected));
     });
 });
 
@@ -52,41 +52,41 @@ test("Verify valid email positive matches", () => {
     const expected = ['EMAIL'];
     
     valid_email.forEach(email => {
-        expect(redact.detect(email)).toEqual(expect.arrayContaining(expected));
+        expect(helper.getTokenTypes(email)).toEqual(expect.arrayContaining(expected));
     });
 });
 
 // Test 4: Verify that the list of supported types is up to date.
 test("Verify supported types are up to date", () => {
     const expected = ['SSN', 'PHONE', 'EMAIL'];
-    expect(redact.getSupportedTypes()).toEqual(expect.arrayContaining(expected));
+    expect(helper.getSupportedTypes()).toEqual(expect.arrayContaining(expected));
 });
 
-// Test 5: Verify that the Redact function returns an empty dictionary when given an empty string.
+// Test 5: Verify that the Redact function returns an empty string when given an empty string.
 test("Verify redact function returns an emtpy dictionary", () => {
-    expect(Object.keys(redact.redact("")).length).toEqual(0);
+    expect(helper.redactString("").length).toEqual(0);
 });
 
-// Test 6: Verify that the Redact function returns a dictionary with a single PII snippet and type.
+// Test 6: Verify that the Redact function returns a redacted string with a single PII snippet.
 test("Verify redact function returns a dictionary with PII snippets and single type", () => {
-    expected = {'(123)456-7890': 'PHONE'}
-    expect(redact.redact("Hello world (123)456-7890")).toEqual(expected);
+    var expected = "Hello world [REDACTED]";
+    expect(helper.redactString("Hello world (123)456-7890")).toEqual(expected);
 });
 
-// Test 7: Verify that the Redact function returns a dictionary with multiple PII types but a single snippet.
+// Test 7: Verify that the Redact function returns a redacted string with multiple PII types but a single snippet.
 test("Verify redact function returns a dictionary with PII snippets and multiple types", () => {
-    expected = {'1234567890': 'SSN,PHONE'}
-    expect(redact.redact("Hello world 1234567890")).toEqual(expected);
+    var expected = "Hello world [REDACTED]";
+    expect(helper.redactString("Hello world 1234567890")).toEqual(expected);
 });
 
 // Test 8: Verify that the Redact function returns a dictionary with multiple PII snippets and types.
 test("Verify redact function returns a dictionary with multiple PII snippets", () => {
-    expected = {'1234567890': 'SSN,PHONE', 'hello@world.com':'EMAIL'}
-    expect(redact.redact("Hello world 1234567890 and my email is hello@world.com")).toEqual(expected);
+    var expected = "Hello world [REDACTED] and my email is [REDACTED]";
+    expect(helper.redactString("Hello world 1234567890 and my email is hello@world.com")).toEqual(expected);
 });
 
 // Test 9: Verify that the Redact function detects only a certain type of PII.
 test("Verify redact function returns a dictionary with only the prompted type of PII", () => {
-    expected = {'hello@world.com':'EMAIL'}
-    expect(redact.redact("Hello world 1234567890 and my email is hello@world.com", ['EMAIL'])).toEqual(expected);
+    var expected = "Hello world 1234567890 and my email is [REDACTED]";
+    expect(helper.redactString("Hello world 1234567890 and my email is hello@world.com", ['EMAIL'])).toEqual(expected);
 });
