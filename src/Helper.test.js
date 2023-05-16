c46af04a79602866b4480d445f934a3ac38ab228
Paul <59670248+impaulb@users.noreply.github.com>
2023-05-16 11:06:13 -0700

    Added support for dates

--- src/Helper.test.js
+++ src/Helper.test.js
@@ -1,6 +1,6 @@
 const helper = require('./Helper.gs');
 
-// Test 1: Verify that valid SSN numbers are detected correctly.
+// Test: Verify that valid SSN numbers are detected correctly.
 test("Verify valid SSN positive matches", () => {
     const valid_ssn = [
         "123-45-6789",
@@ -22,7 +22,7 @@ test("Verify valid SSN positive matches", () => {
     });
 });
 
-// Test 2: Verify that valid phone numbers are detected correctly.
+// Test: Verify that valid phone numbers are detected correctly.
 test("Verify valid phone positive matches", () => {
     const valid_phone = [
         "555-555-5555",
@@ -41,7 +41,25 @@ test("Verify valid phone positive matches", () => {
     });
 });
 
-// Test 3: Verify that valid email addresses are detected correctly.
+// Test: Verify that valid dates are detected correctly.
+test("Verify valid dates positive matches", () => {
+    const valid_date = [
+        "1/1/2002",
+        "01/01/2002",
+        "1.1.2002",
+        "01.01.2002",
+        "1 1 2002",
+        "01 01 2002",
+        "01/01 2002"
+    ];
+    const expected = ['DATE'];
+    
+    valid_date.forEach(date => {
+        expect(helper.getTokenTypes(date)).toEqual(expect.arrayContaining(expected));
+    });
+});
+
+// Test: Verify that valid email addresses are detected correctly.
 test("Verify valid email positive matches", () => {
     const valid_email = [
         "helloworld@gmail.com",
@@ -56,36 +74,36 @@ test("Verify valid email positive matches", () => {
     });
 });
 
-// Test 4: Verify that the list of supported types is up to date.
+// Test: Verify that the list of supported types is up to date.
 test("Verify supported types are up to date", () => {
     const expected = ['SSN', 'PHONE', 'EMAIL'];
     expect(helper.getSupportedTypes()).toEqual(expect.arrayContaining(expected));
 });
 
-// Test 5: Verify that the Redact function returns an empty string when given an empty string.
+// Test: Verify that the Redact function returns an empty string when given an empty string.
 test("Verify redact function returns an emtpy dictionary", () => {
     expect(helper.redactString("").length).toEqual(0);
 });
 
-// Test 6: Verify that the Redact function returns a redacted string with a single PII snippet.
+// Test: Verify that the Redact function returns a redacted string with a single PII snippet.
 test("Verify redact function returns a dictionary with PII snippets and single type", () => {
     var expected = "Hello world [REDACTED]";
     expect(helper.redactString("Hello world (123)456-7890")).toEqual(expected);
 });
 
-// Test 7: Verify that the Redact function returns a redacted string with multiple PII types but a single snippet.
+// Test: Verify that the Redact function returns a redacted string with multiple PII types but a single snippet.
 test("Verify redact function returns a dictionary with PII snippets and multiple types", () => {
     var expected = "Hello world [REDACTED]";
     expect(helper.redactString("Hello world 1234567890")).toEqual(expected);
 });
 
-// Test 8: Verify that the Redact function returns a dictionary with multiple PII snippets and types.
+// Test: Verify that the Redact function returns a dictionary with multiple PII snippets and types.
 test("Verify redact function returns a dictionary with multiple PII snippets", () => {
     var expected = "Hello world [REDACTED] and my email is [REDACTED]";
     expect(helper.redactString("Hello world 1234567890 and my email is hello@world.com")).toEqual(expected);
 });
 
-// Test 9: Verify that the Redact function detects only a certain type of PII.
+// Test: Verify that the Redact function detects only a certain type of PII.
 test("Verify redact function returns a dictionary with only the prompted type of PII", () => {
     var expected = "Hello world 1234567890 and my email is [REDACTED]";
     expect(helper.redactString("Hello world 1234567890 and my email is hello@world.com", ['EMAIL'])).toEqual(expected);
